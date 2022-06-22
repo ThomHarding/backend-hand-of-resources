2ffa7b09bef746972be9be4accb9f87d9714c3d2
THOMAS HARDING <hardingt604@gmail.com>
2022-06-22 17:58:27 -0600

    add hats delete test

--- __tests__/hat.test.js
+++ __tests__/hat.test.js
@@ -36,6 +36,14 @@ describe('hat routes', () => {
     expect(resp.body.name).toBe('Bowler Cap');
   });
 
+  it('DELETE /hats/:id should delete a hat', async () => {
+    const resp = await request(app).delete('/hats/2');
+    expect(resp.status).toEqual(200);
+
+    const { body } = await request(app).get('/hats/2');
+    expect(body).toEqual('');
+  });
+
   afterAll(() => {
     pool.end();
   });
